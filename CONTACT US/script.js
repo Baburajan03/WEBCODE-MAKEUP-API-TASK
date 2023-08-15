ccb4a0bdaf4c2832d4088e7ab4bf6979aae494d2
Baburajan03 <130633153+Baburajan03@users.noreply.github.com>
2023-08-15 17:31:47 +0530

    Add files via upload

---
+++ CONTACT US/script.js
@@ -0,0 +1,36 @@
+let container = document.createElement('div')
+container.classList.add('container')
+document.body.appendChild(container)
+
+let row1 = document.createElement('div')
+row1.classList.add('row','m-4')
+row1.innerHTML = `<ul type="none">
+<li class="ms-3"><a                   href="https://baburajan03.github.io/WEBCODE-MAKEUP-API--TASK/HOME/index.html"><img  class="image mt-3" src="./makeup icon.jpg" ></a></li>
+<li class="ms-3"><a class="underline" href="https://baburajan03.github.io/WEBCODE-MAKEUP-API--TASK/HOME/index.html" >Home</a></li>
+<li class="ms-3"><a class="underline" href="https://baburajan03.github.io/WEBCODE-MAKEUP-API--TASK/BRANDS/index.html">Brands</a></li>
+<li class="ms-3"><a class="underline" href="https://baburajan03.github.io/WEBCODE-MAKEUP-API--TASK/PRODUCT/index.html">Products</a></li>
+<li class="ms-3"><a class="underline" href="https://baburajan03.github.io/WEBCODE-MAKEUP-API--TASK/CONTACT%20US/index.html">ContactUs</a></li>
+</ul>`
+container.appendChild(row1)
+
+let row2 = document.createElement('div')
+row2.classList.add('row','ms-5')
+row2.innerHTML = `<h2>Contact Us</h2>`
+container.appendChild(row2)
+
+let row3 = document.createElement('div')
+row3.classList.add('row','m-3')
+row3.innerHTML = `<div class="col-2 ms-5 mt-3">
+<h5 >Phone</h5>
+<h5 >Email</h5>
+<h5 >Address</h5>
+</div>
+<div class="col-8 ms-1 mt-3">
+<h5>: +123-456-7890</h5>
+<h5>: makeup@website.com</h5>
+<h5>: 123, Anywhere, AnyCountry.</h5>
+<h5></h5>
+</div>
+<div class="col-2">
+</div>`
+container.appendChild(row3)
---
+++ PRODUCTS/script.js
@@ -0,0 +1,118 @@
+let container = document.createElement('div')
+container.classList.add('container')
+document.body.appendChild(container)
+
+let row1 = document.createElement('div')
+row1.classList.add('row','m-4')
+row1.innerHTML = `<ul type="none">
+<li class="ms-3"><a                   href="https://baburajan03.github.io/WEBCODE-MAKEUP-API--TASK/HOME/index.html"><img  class="image mt-3" src="./makeup icon.jpg" ></a></li>
+<li class="ms-3"><a class="underline" href="https://baburajan03.github.io/WEBCODE-MAKEUP-API--TASK/HOME/index.html" >Home</a></li>
+<li class="ms-3"><a class="underline" href="https://baburajan03.github.io/WEBCODE-MAKEUP-API--TASK/BRANDS/index.html">Brands</a></li>
+<li class="ms-3"><a class="underline" href="https://baburajan03.github.io/WEBCODE-MAKEUP-API--TASK/PRODUCT/index.html">Products</a></li>
+<li class="ms-3"><a class="underline" href="https://baburajan03.github.io/WEBCODE-MAKEUP-API--TASK/CONTACT%20US/index.html">ContactUs</a></li>
+</ul>`
+container.appendChild(row1)
+
+let apiUrl = 'https://makeup-api.herokuapp.com/api/v1/products.json';
+
+async function fetchdatas(){
+    try{
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        const productTypes = [...new Set(data.map(product => product.product_type))]
+        return productTypes
+    }
+    catch(error){
+        console.error('Error fetching or processing data:', error);
+        return [];
+    }
+}
+
+async function displayProductTypes() {
+    let productTypes = await fetchdatas()
+
+    var container = document.createElement('div')
+    container.classList.add('container')
+    
+    document.body.appendChild(container)
+
+    var row1= document.createElement('div')
+    row1.classList.add('row')
+    container.appendChild(row1)
+
+    productTypes.forEach(productType => {
+        let col1 = document.createElement('div')
+        col1.classList.add('col','btn','btn-success','m-2')
+        col1.textContent = productType;
+        col1.addEventListener('click', () => {
+            displayResults(productType);
+        })
+        row1.appendChild(col1);
+    })
+    let row2 = document.createElement('div')
+    
+    row2.setAttribute('id','results')
+    row2.classList.add('row')
+    container.appendChild(row2)
+}
+
+async function displayResults(productType) {
+    let response = await fetch(apiUrl)
+    let data = await response.json()
+
+    let filteredProducts = data.filter(product => product.product_type === productType)
+    console.log(filteredProducts)
+
+    let result = document.getElementById('results')
+    result.innerHTML = ''
+
+    filteredProducts.forEach((e) =>{
+        
+    let column1 = document.createElement('div')
+        column1.setAttribute('class','col')
+        result.appendChild(column1)
+        
+        let column2 = document.createElement('div')
+        column1.appendChild(column2) 
+    
+        let column3 = document.createElement('div')
+        column3.classList.add('card','styles')
+        column2.appendChild(column3)
+    
+        let heading1 = document.createElement('h6')
+        heading1.classList.add('card-title')
+        heading1.innerHTML = `<h4>${e.brand}</h4> </br>
+        ${e.product_type}`
+        column3.appendChild(heading1)
+    
+        let image = document.createElement('img')
+        image.setAttribute('src',`${e.image_link}`)
+        image.setAttribute('alt','product image not available')
+        image.classList.add('card-img-top','p-3')
+        column3.appendChild(image)
+    
+        let column4 = document.createElement('div')
+        column4.setAttribute('class','card-body')
+        column3.appendChild(column4)
+    
+        let heading2 = document.createElement('h6')
+        heading2.setAttribute('class','adjust')
+        heading2.innerHTML = `<b>Prize : </b> ${e.price} ${e.price_sign}`
+        column4.appendChild(heading2)
+    
+        let heading3 = document.createElement('h6')
+        heading3.setAttribute('class','adjust')
+        heading3.innerHTML = `<b>Product Link :</b> <a class="underline"  href="${e.product_link}">${e.product_link}</a></br> </br>
+        <b>Website Link :</b> <a class="underline"  href="${e.website_link}">${e.website_link}</a></br></br> <b>Description :</b> ${e.description}`
+        column4.appendChild(heading3)
+    
+        let button = document.createElement('button')
+        button.classList.add('btn','btn-outline-secondary')
+        button.innerHTML = "Buy Now"
+        column4.appendChild(button)
+        result.appendChild(column1)
+    })
+    }
+
+displayProductTypes()
